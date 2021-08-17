5198820fb4e95af6b2c61563258fdd123ed9ba3c
iamvarshith <50661062+iamvarshith@users.noreply.github.com>
2021-08-17 23:23:25 +0530

    Sliding Window approach

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 The problems solved under these patterns use a varied set of algorithmic techniques.
 We will make use of Breadth-First Search and Depth-First Search to solve problems related to Trees and Graphs. 
 Similarly, we will also cover Dynamic Programming, Backtracking, Recursion, Greedy algorithms, and Divide &amp; Conquer.
+------------
 
+# Detailed Explanation in my blog please check.
+## [iamvarshith.dev](http://iamvarshith.dev "iamvarshith.dev") 
 
 ------------
 
--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-string1 = "Python3,Flask-Microframework,Jinja2,Flask_WTF,Flask_Login,Flask_Admin,WTForms,requests,Flask_SQLAlchemy,Flask,pycryptodomex"
+def maxSlidingWindow(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: List[int]
+    """
+    maxi = []
+    sum_found = 0
+    window_start = 0
+    for window_end in range(len(nums)):
+        sum_found = nums[window_end] + sum_found
+
+        if window_end >= k - 1:
+            print(sum_found, )
+            maxi.append(sum_found)
+            left_char = nums[window_start]
+            sum_found -= left_char
+            window_start += 1
+    return maxi
 
 
-print(string1.replace(' ', ''))
+print(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
