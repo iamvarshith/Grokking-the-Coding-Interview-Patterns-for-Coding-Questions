065cc3a51621be17619f2a0663bd70a11327ba20
iamvarshith <50661062+iamvarshith@users.noreply.github.com>
2021-08-22 00:48:13 +0530

    two pointer approach

---
+++ Two_Pointer/Remove_Duplicates_Problem_2.py
@@ -0,0 +1,30 @@
+"""
+Problem 1: Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place and return the new length of the array.
+
+Example 1:
+
+Input: [3, 2, 3, 6, 3, 10, 9, 3], Key=3
+Output: 4
+Explanation: The first four elements after removing every 'Key' will be [2, 6, 10, 9].
+
+Example 2:
+
+Input: [2, 11, 2, 2, 1], Key=2
+Output: 2
+Explanation: The first two elements after removing every 'Key' will be [11, 1].
+
+"""
+
+
+def remove_duplicates_given(arr, key):
+    key_element = 0
+
+    for index in range(len(arr)):
+        if arr[index] != key:
+            arr[key_element] = arr[index]
+            key_element += 1
+    return key_element
+
+
+print(remove_duplicates_given([3, 2, 3, 6, 3, 10, 9, 3], 3))
+print(remove_duplicates_given([2, 11, 2, 2, 1], 2))
