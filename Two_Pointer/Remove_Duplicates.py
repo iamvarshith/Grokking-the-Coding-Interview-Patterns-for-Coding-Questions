2f5935ca90e6f1c56ea89e28acdf993ce3b504b1
iamvarshith <50661062+iamvarshith@users.noreply.github.com>
2021-08-21 23:18:42 +0530

    two pointer approach

--- Two_Pointer/Remove_Duplicates.py
+++ Two_Pointer/Remove_Duplicates.py
@@ -16,15 +16,26 @@ Output: 2
 Explanation: The first two elements after removing the duplicates will be [2, 11]."""
 
 
+# This is will give you the solution but will not change the list !. So the tak in hand is to change the array
+#
+# def remove_duplicates(arr):
+#     duplicates = 0
+#
+#     for pointer_start in range(1, len(arr)):
+#         pointer_end = pointer_start - 1
+#         if arr[pointer_start] == arr[pointer_end]:
+#             duplicates += 1
+#     return len(arr) - duplicates
+#
+#
+# print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+# print(remove_duplicates([2, 2, 2, 11]))
+
 def remove_duplicates(arr):
-    duplicates = 0
-
-    for pointer_start in range(1, len(arr)):
-        pointer_end = pointer_start - 1
-        if arr[pointer_start] == arr[pointer_end]:
-            duplicates += 1
-    return len(arr) - duplicates
-
-
-print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
-print(remove_duplicates([2, 2, 2, 11]))
+    non_duplicate = 1
+    pointer = 1
+    while (pointer < len(arr)):
+        if arr[non_duplicate - 1] != arr[pointer]:
+            arr[non_duplicate] = arr[pointer]
+            non_duplicate += 1
+    return non_duplicate
