52b0ebc1fed796873fe273ed3041e08359e143f0
iamvarshith <varshithmuthya.vankamamidi18@st.niituniversity.in>
2022-01-04 08:23:47 +0530

    Day 1 Triplet_Sum_to_Zero.py

--- Two_Pointer/Triplet_Sum_to_Zero.py
+++ Two_Pointer/Triplet_Sum_to_Zero.py
@@ -1,5 +1,5 @@
 """
-Problem Statement ##
+Problem Statement #
 
 Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
 
@@ -18,38 +18,40 @@ Explanation: There are two unique triplets whose sum is equal to zero.
 """
 
 
-def search_triplets(arr):
+def search_triplets(arr: list):
+    triplet_list = []
     arr.sort()
-    triplets = []
+    print(arr)
     for i in range(len(arr)):
-        if i > 0 and arr[i] == arr[i - 1]:  # skip same element to avoid duplicate triplets
+        if i > 0 and arr[i] == arr[i - 1]:
             continue
-        search_pair(arr, -arr[i], i + 1, triplets)
-
-    return triplets
-
-
-def search_pair(arr, target_sum, left, triplets):
-    right = len(arr) - 1
-    while (left < right):
-        current_sum = arr[left] + arr[right]
-        if current_sum == target_sum:  # found the triplet
-            triplets.append([-target_sum, arr[left], arr[right]])
-            left += 1
-            right -= 1
-            while left < right and arr[left] == arr[left - 1]:
-                left += 1  # skip same element to avoid duplicate triplets
-            while left < right and arr[right] == arr[right + 1]:
-                right -= 1  # skip same element to avoid duplicate triplets
-        elif target_sum > current_sum:
-            left += 1  # we need a pair with a bigger sum
-        else:
-            right -= 1  # we need a pair with a smaller sum
+        search_2sum(arr, i, triplet_list)
+    return triplet_list
 
 
-def main():
+def search_2sum(arr, index, triplet_list):
+    target_sum = -arr[index]
+
+    start_pointer = index + 1
+    end_pointer = len(arr) - 1
+
+    while start_pointer < end_pointer:
+        two_sum = arr[start_pointer] + arr[end_pointer]
+
+        if two_sum < target_sum:
+            start_pointer += 1
+        elif two_sum > target_sum:
+            end_pointer -= 1
+        elif two_sum == target_sum:
+            found = [arr[index], arr[start_pointer], arr[end_pointer]]
+            triplet_list.append(found)
+            start_pointer += 1
+            end_pointer -= 1
+            while start_pointer < end_pointer and arr[start_pointer] == arr[start_pointer - 1]:
+                start_pointer += 1
+            while start_pointer < end_pointer and arr[end_pointer] == arr[end_pointer + 1]:
+                end_pointer -= 1
+
+
+if __name__ == "__main__":
     print(search_triplets([-3, 0, 1, 2, -1, 1, -2]))
-    print(search_triplets([-5, 2, -1, -2, 3]))
-
-
-main()
